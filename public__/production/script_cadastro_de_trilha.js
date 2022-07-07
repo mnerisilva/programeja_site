beed09a3fab0478f81bbe1d54c5bdbe7858938af
mnerisilva <marceloneri@gmail.com>
2022-07-07 16:37:53 -0300

    update

---
+++ public__/production/script_cadastro_de_trilha.js
@@ -0,0 +1,8 @@
+
+                          const _formCadastroDeTrilha = document.querySelector('.form-cadastro-de-trilha');
+                         
+                            $(_formCadastroDeTrilha).submit(function(event){
+                                event.preventDefault();
+                                console.log('entrou no submit do form-cadastro de trilhas')
+                            
+                          });
